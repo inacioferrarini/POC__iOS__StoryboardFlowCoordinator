51cc23abc1c5823747b2e1e1f43c34cc18a830ad
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2016-11-27 19:10:15 -0200

    Adding model object

---
+++ StoryboardFlowCoordinator/User/Model/UserPFData.swift
@@ -0,0 +1,17 @@
+//
+//  PFUserData.swift
+//  StoryboardFlowCoordinator
+//
+//  Created by Inácio on 27/11/16.
+//  Copyright © 2016 com.inacioferrarini.gh. All rights reserved.
+//
+
+import UIKit
+
+class UserPFData {
+
+    var name: String?
+    var cpf: String?
+    var email: String?
+
+}
